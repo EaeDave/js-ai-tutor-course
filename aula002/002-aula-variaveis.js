c59ab0ea1aaedcc6ef8925a03a458ce1cf3c21d2
EaeDave <eudaverdgs@gmail.com>
2025-06-17 11:30:13 -0300

    Aula sobre variaveis (let e const)

---
+++ aula002/002-aula-variaveis.js
@@ -0,0 +1,10 @@
+let personagem = 'Naruto Uzumaki';  // Variáveis são formas de armazenar valores na memória e poder consultá-los de forma rápida.
+console.log(personagem);
+personagem = 'Kakashi Hatake'  // Valor da variável foi atualizado
+console.log(personagem);
+
+const vila = 'Aldeia da Folha';  // Constantes também permite armazenar valores na memória da mesma forma, porém sem poder alterar posteriormente.
+console.log(vila);
+
+// vila = 'Aldeia da Areia';  // Tentando mudar uma constante = Gerar um TypeError "Assignment to constant variable"
+
