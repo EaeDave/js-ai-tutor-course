5cdfebf1e2efbad90c26a5b237394d62cda02020
EaeDave <eudaverdgs@gmail.com>
2025-06-17 11:36:59 -0300

    Exercício | Atualizando perfil Ninja com variáveis e constantes!

---
+++ aula002/002-exercicio-perfil-ninja.js
@@ -0,0 +1,17 @@
+const nome = 'Sakura';
+const cla = 'Haruno';
+const vila = 'Aldeia da Folha';
+let rankNinja = 'Gennin';
+let jutsuPrincipal = 'Nenhum';
+
+console.log(nome);
+console.log(cla);
+console.log(vila);
+console.log(rankNinja);
+console.log(jutsuPrincipal);
+
+rankNinja = 'Chunnin';
+jutsuPrincipal = 'Técnica da Palma Mística';
+
+console.log(rankNinja);
+console.log(jutsuPrincipal);
