830754529e7f8f1e1ce82e4d2a29c1973958c6b8
EaeDave <eudaverdgs@gmail.com>
2025-06-18 09:31:58 -0300

    Exercício | Tipos de dados - Missões ninja

---
+++ aula003/003-exercicio-missoes-ninja.js
@@ -0,0 +1,13 @@
+const nomeMissao = 'Resgatar Kazekage';
+const recompensa = 100000;
+let ninjaEnvolvidos = 4;
+let concluida = false;
+
+console.log('Nome da missão: ', nomeMissao);
+console.log('Recompensa: ', recompensa);
+console.log('Ninjas envolvidos: ', ninjaEnvolvidos);
+console.log('Concluída? ', concluida);
+
+concluida = true;
+
+console.log("A missão 'Resgatar o Kazekage' foi concluída?", concluida);
