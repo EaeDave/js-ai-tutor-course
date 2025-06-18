209d1b6512eedf7ceb6850af64606f6bfb018ea2
EaeDave <eudaverdgs@gmail.com>
2025-06-18 10:01:23 -0300

    Exercício | Depuração e correção de bug no script da Akatsuki

---
+++ aula004/004-exercicio-depuracao-akatsuki.js
@@ -0,0 +1,13 @@
+// O objetivo é criar uma lista de membros da Akatsuki,
+// exibir o terceiro membro e depois adicionar um novo.
+
+const akatsuki = ['Deidara', 'Konan', 'Pain'];
+
+console.log('O terceiro membro é:');
+console.log(akatsuki[2]); // Tentando acessar o terceiro membro
+
+// Adicionando um novo membro
+akatsuki.push('Zetsu');
+
+console.log('Lista final da Akatsuki:');
+console.log(akatsuki);
