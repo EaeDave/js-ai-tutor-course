05517de940620859dbefc0b21af5e337e8d68a76
EaeDave <eudaverdgs@gmail.com>
2025-06-18 09:55:57 -0300

    Exercício | Manipulando array - Lista de Kages

---
+++ aula004/004-exercicio-kages.js
@@ -0,0 +1,9 @@
+const kages = ['A', 'Onoki', 'Mei Terumi', 'Danzo Shimura'];
+console.log('Kages iniciais: ', kages);
+kages[3] = 'Tsunade Senju';
+kages.push('Gaara');
+console.log('Kages corrigidos: ', kages);
+qtdKages = kages.length;
+console.log('A aliança agora tem', qtdKages, 'Kages.');
+
+
