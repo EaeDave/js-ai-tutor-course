8555be5fb4710a778a672da7fc23b014437b71d6
EaeDave <eudaverdgs@gmail.com>
2025-06-18 09:22:50 -0300

    Tipos de dados

---
+++ aula003/003-aula-tipos-de-dados.js
@@ -0,0 +1,23 @@
+const jutsu1 = 'Rasengan';
+const jutsu2 = 'Chidori';
+
+const confronto = jutsu1 + ' vs ' + jutsu2;
+
+console.log(confronto);
+
+const ninjasNaAcademia = 120;
+const ninjasAprovados = 9;
+
+const ninjasReprovados = ninjasNaAcademia - ninjasAprovados;
+
+console.log('Número de ninjas reprovados no teste de sinos:');
+console.log(ninjasReprovados);
+
+const narutoEhJinchuriki = true;
+const kakashiEhJinchuriki = false;
+
+console.log('Naruto é o Jinchuriki da Nove-Caudas?');
+console.log(narutoEhJinchuriki);
+
+console.log('Kakashi é o Jinchuriki da Nove-Caudas?');
+console.log(kakashiEhJinchuriki);
